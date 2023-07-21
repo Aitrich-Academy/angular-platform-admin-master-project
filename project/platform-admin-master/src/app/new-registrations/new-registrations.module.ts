46fcdbcfc3c5e153a1af929cc86901f53186017c
shini parameswaran <shinip.aitrich@gmail.com>
2023-07-21 15:50:03 +0530

    components added

--- project/platform-admin-master/src/app/new-registrations/new-registrations.module.ts
+++ project/platform-admin-master/src/app/new-registrations/new-registrations.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewRegistrationComponent } from './components/new-registration/new-registration.component';
+import { NewRegistrationHomeComponent } from './components/new-registration-home/new-registration-home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewRegistrationComponent,
+    NewRegistrationHomeComponent
+  ],
   imports: [
     CommonModule
   ]
